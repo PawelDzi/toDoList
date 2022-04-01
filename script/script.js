d8fcba3064bb1711eeb8f896967abfbf3d62703c
Pawel Dzierzbicki <ativek23@gmail.com>
2022-04-01 13:40:30 +0200

    toggle task done

--- script/script.js
+++ script/script.js
@@ -11,7 +11,12 @@
 
   const removeTask = (taskIndex) => {
     tasks.splice(taskIndex, 1);
-      render();
+    render();
+  };
+
+  const toggleTaskDone = (taskIndex) => {
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    render();
   };
 
   const render = () => {
@@ -20,6 +25,7 @@
     for (const task of tasks) {
       htmlString += `
         <li ${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+        <button class="js-done">zrobione</button>
         ${task.content}
         <button class="js-remove">usuń</button>
         </li>
@@ -33,6 +39,14 @@
     removeButtons.forEach((removeButtons, index) => {
       removeButtons.addEventListener("click", () => {
         removeTask(index);
+      });
+    });
+
+    const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+    toggleDoneButtons.forEach((toggleDoneButtons, index) => {
+      toggleDoneButtons.addEventListener("click", () => {
+        toggleTaskDone(index);
       });
     });
   };
